f4e66b79a37e7981c1590f9b09baba0d83adc67a
mohammed alsamaki <samkimohd@gmail.com>
2024-04-06 13:42:44 +0300

    Initial commit

---
+++ bin/dart_intro_2024.dart
@@ -0,0 +1,5 @@
+import 'package:dart_intro_2024/dart_intro_2024.dart' as dart_intro_2024;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_intro_2024.calculate()}!');
+}
---
+++ test/dart_intro_2024_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_intro_2024/dart_intro_2024.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
