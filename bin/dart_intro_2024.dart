80ba029bede728529daa5020de841fadef0b3050
mohammed alsamaki <samkimohd@gmail.com>
2024-04-06 14:41:43 +0300

    learn about data types

--- bin/dart_intro_2024.dart
+++ bin/dart_intro_2024.dart
@@ -1,5 +1,45 @@
-import 'package:dart_intro_2024/dart_intro_2024.dart' as dart_intro_2024;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_intro_2024.calculate()}!');
+void main() {
+//   comment :
+  String username = 'ali';
+  print(username);
+  //  int  : integer any number can be used without decimal point
+  int age = 25;
+  print(age);
+  //  double : double number can be used with decimal point
+  double height = 1.80;
+  print(height);
+  //  bool : true or false
+  bool isBool = true;
+  print(isBool);
+  //  dynamic : variable can be changed at any time
+  dynamic dynamicVar = 10;
+  print(dynamicVar);
+  dynamicVar = 'any string';
+  print(dynamicVar);
+
+  //  array and list : one variable can contain multiple values
+  List myList = [1, 'hi', true];
+
+  print(myList);
+  //  index : the value location in the list and start from 0
+  List<int> myArray = [1, 2, 3, 4, 5];
+  print(myArray[2]);
+
+  //  for loop : run code for a specific number of times
+  //  start :> condition :> increment or decrement
+  for (int i = 0; i < myArray.length; i++) {
+    //   print(myArray[4]);
+    print(myArray[i]);
+  }
+
+  //  Map (object,dictionary) : var contains many keys and values
+  Map person = {'name': 'ahmad', 'age': 20, 'city': 'cairo'};
+  print('person name  : ${person['name']}');
+  //  null safty :  ?      !
+  int? x;
+  print(x);
+  x = 10;
+  print(x);
 }
